4863d99bdc5eb1b7e0176650e5f78199c1b105d0
Karina Evers <krin_11_bball@yahoo.com>
2019-06-19 20:46:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,8 +5,11 @@ let playlist = {
 
 function updatePlaylist(playlist, artist, songTitle) {
   playlist[artist='Phil Ochs']=[songTitle='My Bloody Valentine']
+  return playlist
 }
+
 
 function removeFromPlaylist(playlist, artist){
   delete playlist[artist='Slowdive'];
+  return playlist
 }
